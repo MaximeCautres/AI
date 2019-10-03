1a22a0d504c19bc1d1881a04a6973651b6b5ed24
Maxime Cautrès <maxime.cautres@gmail.com>
2019-10-03 23:25:44 +0200

    seance tipe du 3/10/19

--- reinforcement_learning/tipe/mdp.py
+++ reinforcement_learning/tipe/mdp.py
@@ -1,12 +1,44 @@
 import numpy as np
 
-states_count = 6
-actions_count = 6
+alpha = 0.9
+gamma = 0.8
+
+w = 5 + 2
+h = 7 + 2
+p = 0
+
+begin = (1, 1)
+goal = (h-2, w-2)
+
+maps = np.array([[not (np.random.random() < p or i == 0 or i == w-1 or j == 0 or j == h-1) for i in range(w)] for j in range(h)])
+actions_set = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+
+states_count = h * w
+actions_count = len(actions_set)
 step_count = 6
 keep_count = 6
 
-alpha = 0.9
-gamma = 0.8
+
+def generate_world_mouse():
+    actions = np.zeros((states_count, actions_count))
+    transitions = np.zeros((states_count, actions_count, states_count))
+    rewards = np.zeros((states_count, actions_count, states_count))
+
+    for j in range(1, h - 1):
+        for i in range(1, w - 1):
+            for k in range(actions_count):
+                y, x = actions_set[k]
+                actions[j * w + i, k] = maps[j + y, i + x] * 1
+                transitions[j * w + i, k, (j + y) * w + i + x] = maps[j + y, i + x] * 1
+                rewards[j * w + i, k, (j + y) * w + i + x] = (((j + y) * w, i + x) == goal) * states_count - 1
+
+    return {'actions': actions, 'transitions': transitions, 'rewards': rewards}
+
+
+def initialize_agent_mouse():
+    current = w + 1
+    values = np.random.random((states_count, actions_count, states_count))
+    return {'current': current, 'values': values}
 
 
 def generate_world():
@@ -33,6 +65,7 @@ def policy(vs, s):
 def make_a_step():
     state = agent['current']
     action = policy(current_values, state)
+    print(action)
     state_prime = np.random.choice(states_count, p=world['transitions'][state, action])
     instant_reward = world['rewards'][state, action, state_prime]
 
@@ -43,11 +76,13 @@ def make_a_step():
     agent['values'][state, action, state_prime] += alpha * new
 
 
-world = generate_world()
-agent = initialize_agent()
+world = generate_world_mouse()
+agent = initialize_agent_mouse()
 current_values = None
 
 for t in range(step_count):
     if not t % keep_count:
         current_values = agent['values']
+    v = agent['current']
+    print((v // w, v % w))
     make_a_step()
