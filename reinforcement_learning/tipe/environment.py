f680cf49f8acd27ad4e8e2c47f5b7980afb269ef
MaximeCautres <maxime.cautres@gmail.com>
2019-12-05 17:02:25 +0100

    Seance du 5/12/19

--- reinforcement_learning/tipe/environment.py
+++ reinforcement_learning/tipe/environment.py
@@ -3,8 +3,8 @@ import pygame
 import numpy as np
 
 RED = 1 / 3
-GREEN = 1
-BLUE = 2 / 3
+GREEN = 2 / 3
+BLUE = 1
 
 topology = (24*24*2, 128, 9)
 parameters = {}
@@ -16,23 +16,67 @@ def initialize_parameters():
     for l in range(1, parameters['L']):
         cond = l < parameters['L'] - 1
         parameters['af' + str(l)] = 'relu' * cond + 'softmax' * (not cond)
-        parameters['w' + str(l)] = np.random.randn(*topology[l-1:l+1][::-1]) * 10**-2
+        parameters['w' + str(l)] = np.random.randn(*topology[l-1:l+1][::-1]) * 10**-1
         parameters['b' + str(l)] = np.zeros((topology[l], 1))
 
 
-def forward(x):
+def forward(x, return_cache=False):
     a = x
+    cache = {'a0': a}
+
     for l in range(1, parameters['L']):
-        z = np.dot(parameters['w' + str(l)], a) + parameters['b' + str(l)]
-        if parameters['af' + str(l)] == 'relu':
+        w = parameters['w' + str(l)]
+        b = parameters['b' + str(l)]
+        af = parameters['af' + str(l)]
+
+        z = np.dot(w, a) + b
+        if af == 'relu':
             a = relu(z)
-        elif parameters['af' + str(l)] == 'softmax':
+        elif af == 'softmax':
             a = softmax(z)
-    return a
+
+        cache['z' + str(l)] = z
+        cache['a' + str(l)] = a
+
+    if return_cache:
+        return cache
+    else:
+        return a
 
 
-def backward(dy):
-    return dy
+def backward(x, y):
+    gradients = {}
+    n = x.shape[1]
+    cache = forward(x, True)
+    y_hat = cache['a' + str(parameters['L']-1)]
+
+    da = np.divide(1 - y, 1 - y_hat) - np.divide(y, y_hat)
+    dz = None
+
+    for l in reversed(range(1, parameters['L'])):
+        z = cache['z' + str(l)]
+        af = parameters['af' + str(l)]
+
+        if af == 'relu':
+            dz = da * relu_prime(z)
+        elif af == 'softmax':
+            dz = y_hat - y
+
+        a_prev = cache['a' + str(l - 1)]
+        w = parameters['w' + str(l)]
+
+        gradients['dw' + str(l)] = (1 / n) * np.dot(dz, a_prev.T)
+        gradients['db' + str(l)] = (1 / n) * np.sum(dz, axis=1, keepdims=True)
+
+        da = np.dot(w.T, dz)
+
+    return gradients
+
+
+def update_parameters(gradients, alpha):
+    for l in range(1, parameters['L']):
+        parameters['w' + str(l)] -= alpha * gradients['dw' + str(l)]
+        parameters['b' + str(l)] -= alpha * gradients['db' + str(l)]
 
 
 def relu(z):
@@ -97,7 +141,7 @@ class Rectangle:
         return cond and self.convex or not (cond or self.convex)
 
 
-def simulation(dims, obstacles, p0, v0, goal, goal_radius, life_time, episode_length, epoch_count, unit=None):
+def simulation(dims, obstacles, v0, goal, goal_radius, life_time, episode_length, epoch_count, unit=None):
 
     class Boat:
 
@@ -115,17 +159,32 @@ def simulation(dims, obstacles, p0, v0, goal, goal_radius, life_time, episode_le
                 self.pos = new_pos
                 return self.pos.dist(goal)
 
+    def type_touched(v):
+        if not v:
+            return 'pasouf'
+        if v.dist(goal) < goal_radius:
+            return 'goal'
+        for obs in obstacles:
+            if obs.convex and obs.collide(v):
+                return 'obs'
+
     def create_background():
         bg = np.zeros(dims.coord)
         for i in range(dims.x):
             for j in range(dims.y):
                 point = Vec2(i, j)
-                if point.dist(goal) < goal_radius:
+                type_t = type_touched(point)
+                if type_t == 'obs':
+                    bg[i, j] = RED
+                elif type_t == 'goal':
                     bg[i, j] = GREEN
-                for obs in obstacles:
-                    if obs.convex and obs.collide(point):
-                        bg[i, j] = RED
         return bg
+
+    def get_pos():
+        pos = None
+        while type_touched(pos):
+            pos = Vec2(np.random.randint(dims.x), np.random.randint(dims.y))
+        return pos
 
     def create_data_set(dat, n):
         f_stack, l_stack = np.zeros((1152, n)), np.zeros((9, n))
@@ -166,23 +225,29 @@ def simulation(dims, obstacles, p0, v0, goal, goal_radius, life_time, episode_le
         return pygame.transform.scale(surface, size)
 
     def makone():
-        billy.pos = p0
+        billy.pos = get_pos()
         billy.vel = v0
         episode_couples = []
         game_state = "in_game"
         life = 0
+        previous = current_frame(billy.pos)
+        current = previous
 
         if unit:
             pygame.init()
             img_size = (dims.scale_prod(unit)).coord
             screen = pygame.display.set_mode(img_size)
             while game_state == "in_game" and life < life_time:
-                current = current_frame(billy.pos)
                 screen.blit(numpy_to_pygame(current, img_size), (0, 0))
                 for event in pygame.event.get():
                     if event.type == pygame.MOUSEBUTTONDOWN:
-                        action = random.choice(actions)
-                        print(action)
+                        current = current_frame(billy.pos)
+                        array = np.append(np.reshape(previous, 576), np.reshape(current, 576)).reshape(1152, 1)
+                        a = forward(array)
+                        print(a)
+                        episode_couples.append((array, a))
+                        action = actions[int(np.argmax(a))]
+                        previous = current
                         life += 1
                         res = billy.apply_action(action)
                         if res is None:
@@ -190,10 +255,8 @@ def simulation(dims, obstacles, p0, v0, goal, goal_radius, life_time, episode_le
                         elif res < goal_radius:
                             game_state = "win"
                 pygame.display.flip()
-            print(game_state)
             pygame.quit()
         else:
-            previous = current_frame(billy.pos)
             while game_state == "in_game" and life < life_time:
                 current = current_frame(billy.pos)
                 array = np.append(np.reshape(previous, 576), np.reshape(current, 576)).reshape(1152, 1)
@@ -217,13 +280,18 @@ def simulation(dims, obstacles, p0, v0, goal, goal_radius, life_time, episode_le
     for epoch in range(epoch_count):
         data = []
         count = 0
+        win_count = 0
         for episode in range(episode_length):
             result, couples = makone()
             if result != "in_game":
                 count += len(couples)
                 data.append((result, couples))
+                if result == 'win':
+                    win_count += 1
         data_set = create_data_set(data, count)
-        print(forward(data_set[0]))
+        grads = backward(*data_set)
+        update_parameters(grads, 10**-1)
+        print(win_count / episode_length)
 
 
 initialize_parameters()
@@ -231,6 +299,6 @@ dimension = Vec2(24, 24)
 rectangles = [Rectangle(Vec2(0, dimension.y-1), Vec2(dimension.x-1, 0), False),
               Rectangle(Vec2(3, 17), Vec2(5, 15)),
               Rectangle(Vec2(15, 5), Vec2(17, 3))]
-init_pos = Vec2(dimension.x // 3, dimension.y // 3)
 goal_pos = Vec2(2 * dimension.x // 3, 2 * dimension.y // 3)
-simulation(dimension, rectangles, init_pos, Vec2(), goal_pos, 6, 10, 6, 6)
+simulation(dimension, rectangles, Vec2(), goal_pos, 6, 10, 128, 128)
+simulation(dimension, rectangles, Vec2(), goal_pos, 6, 10, 1, 6, 36)
